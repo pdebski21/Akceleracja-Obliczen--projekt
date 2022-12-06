eef26805e1352e5cc1e5f62c4d4b1c5006ccc610
AgataRubaszewska <80752406+AgataRubaszewska@users.noreply.github.com>
2022-12-06 14:13:45 +0100

    Add files via upload

--- perf/run_comparison.py
+++ perf/run_comparison.py
@@ -3,6 +3,7 @@ import writer
 from mandelbrot_cpu.with_colors.mandelbrot import draw_mandelbrot_set as mandelbrot_cpu
 from mandelbrot_cpu.with_colors.mandelbrot_parallel import draw_mandelbrot_set as mandelbrot_cpu_parallel
 from mandelbort_gpu.with_colors.mandelbrot import draw_mandelbrot_set as mandelbrot_gpu
+from mandelbort_gpu.with_colors.withoutCompilation.mandelbrot import draw_mandelbrot_set as mandelbrot_gpu_onlyComputation
 
 
 def main():
@@ -20,20 +21,24 @@ def main():
         img_output_cpu = config['result']['img_output_cpu']
         img_output_cpu_parallel = config['result']['img_output_cpu_parallel']
         img_output_gpu = config['result']['img_output_gpu']
+        img_output_gpu_withoutCompilation = config['result']['img_output_gpu_withoutCompilation']
 
         mandelbrot_cpu_results = {}
         mandelbrot_cpu_parallel_results = {}
         mandelbrot_gpu_results = {}
+        mandelbrot_gpu_withoutCompilation_results = {}
         for iteration in iterations:
             cpu_time = mandelbrot_cpu(iteration, width, height, img_output_cpu)
             cpu_parallel_time = mandelbrot_cpu_parallel(iteration, width, height, img_output_cpu_parallel)
             gpu_time = mandelbrot_gpu(iteration, width, height, img_output_gpu)
+            gpu_without_compilation_time = mandelbrot_gpu_onlyComputation(iteration, width, height,img_output_gpu_withoutCompilation)
             mandelbrot_cpu_results[iteration] = cpu_time
             mandelbrot_cpu_parallel_results[iteration] = cpu_parallel_time
             mandelbrot_gpu_results[iteration] = gpu_time
+            mandelbrot_gpu_withoutCompilation_results[iteration] = gpu_without_compilation_time
 
         writer.write_to_csv(output_file, [width, height], mandelbrot_cpu_results,
-                            mandelbrot_cpu_parallel_results, mandelbrot_gpu_results)
+                            mandelbrot_cpu_parallel_results, mandelbrot_gpu_results, mandelbrot_gpu_withoutCompilation_results)
 
     except OSError:
         print("Something wrong in init file")
--- perf/writer.py
+++ perf/writer.py
@@ -1,9 +1,9 @@
 import csv
 
 
-def write_to_csv(out_file_name, resolution,  time_cpu, time_cpu_parallel, time_gpu):
+def write_to_csv(out_file_name, resolution,  time_cpu, time_cpu_parallel, time_gpu, time_gpu_withoutCompilation):
     out_file = open(out_file_name, 'w', newline='')
-    headers = ['instance_size', 'resolution', 'time_cpu', 'time_cpu_parallel', 'time_gpu']
+    headers = ['instance_size', 'resolution', 'time_cpu', 'time_cpu_parallel', 'time_gpu', 'time_gpu_only_computation']
     writer = csv.DictWriter(out_file, delimiter=';', lineterminator='\n', fieldnames=headers)
 
     writer.writeheader()
@@ -13,7 +13,8 @@ def write_to_csv(out_file_name, resolution,  time_cpu, time_cpu_parallel, time_g
                          'resolution': str(resolution),
                          'time_cpu': str(list(time_cpu.values())[i]).replace('.', ','),
                          'time_cpu_parallel': str(list(time_cpu_parallel.values())[i]).replace('.', ','),
-                         'time_gpu': str(list(time_gpu.values())[i]).replace('.', ',')
+                         'time_gpu': str(list(time_gpu.values())[i]).replace('.', ','),
+                         'time_gpu_only_computation': str(list(time_gpu_withoutCompilation.values())[i]).replace('.', ',')
                          })
 
     out_file.close()
